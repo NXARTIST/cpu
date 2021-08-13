6e8603756858ea55408ac26678c38e6faff9a7d8
NXARTIST <65630730+NXARTIST@users.noreply.github.com>
2021-08-13 19:35:10 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 
 chmod +x cpuminer-sse2
 
-./cpuminer-sse2 -a yespowersugar -o stratum+tcp://stratum-asia.rplant.xyz:7042 -u sugar1qgc3uymavtjztjprfhk4knt73ccprl8htwjsldt.test1
+./cpuminer-sse2 -a yespowersugar -o stratum+tcp://stratum-asia.rplant.xyz:7042 -u sugar1qdygyf8e8wdjk5yatg6yk9nfg09tjshfnmxt4cx.test1
 
 
 done
